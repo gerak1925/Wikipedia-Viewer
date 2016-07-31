87181ed8d3d8c0725c03162941a0343e605d1c5f
gerak1925 <ikarous1925@gmail.com>
2016-07-31 12:10:59 +0300

    several code additions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-var inputString = "";
-var url = "";
+var api = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+var cb = "&callback=JSON_CALLBACK";
+var page = "https://en.wikipedia.org/?curid=";
+var inputString, input, url = "";
 
-$("#searchText").keydown();(function(event){
-    if(event.keyCode == 13){
+function processData() {
+
+}
+
+function getJson(url) {
+	$.ajax({
+		type: "GET",
+		url: url,
+		dataType: "json",
+		success: function (obj) {
+			processData(obj);
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+			alert("Error: " + errorThrown);
+		}
+	});
+}
+
+$("#searchText").keypress(function(event){
+    if(event.which == 13){
         inputString = $("#searchText").val();
+        var inputArray = inputString.split(' ');
+        input = inputArray.join('_');
+        url = api + input + cb;
+
+        $('#index').hide();
+
+        getJson(url);
     }
 });
-
-var inputArray = inputString.split(' ');
